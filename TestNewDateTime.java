4a33f0dda984017e3cc90765ab8ec24f58209a1f
Jaya S <jayalakshmi3214@gmail.com>
2025-05-01 13:21:20 +0530

    Adding TestOptional & TestDateTime Java files

---
+++ TestNewDateTime.java
@@ -0,0 +1,23 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
+
+public class TestNewDateTime {
+	public static void main(String args[]){
+		LocalDate date = LocalDate.now();
+		System.out.println("Current Date -> " + date);
+
+		LocalTime time = LocalTime.now();
+		System.out.println("Current Time -> " + time);
+
+		LocalDateTime dateTime = LocalDateTime.now();
+		System.out.println("Current Date and Time -> " + dateTime);
+
+		LocalDate customDate = LocalDate.of(1999,6,14);
+		System.out.println("Custom Date -> " + customDate);
+
+		DateTimeFormatter dt1 = DateTimeFormater.of("MM/dd/yyyy");
+		DateTimeFormatter dt2 = DateTimeFormatter.of("dd-MM-yyyy");
+		System.out.println("Printing Date ")
+	}
+}
