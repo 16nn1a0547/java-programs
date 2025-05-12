f2438b7fa5b83df592eb930ae99e41998f44c711
Jaya S <jayalakshmi3214@gmail.com>
2025-05-12 22:27:29 +0530

    Adding TestConditional.java

---
+++ TestConditional.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class TestConditional {
+	public static void main(String args[]){
+		int a=5;
+		if(a>10){
+			System.out.println("Number greater than 10");
+		} else {
+			System.out.println("Number less than 10");
+		}
+	}
+}
